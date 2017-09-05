a296a6ff22272e76eecde0e3deff181477bab342
Joe Bowbeer <joe.bowbeer@gmail.com>
2017-09-04 18:46:12 -0700

    BleMidiEncoder refactor

--- CuriePipe.ino
+++ CuriePipe.ino
@@ -13,6 +13,7 @@
 #include <Wire.h> // required by OpenPipe
 #include <OpenPipe.h>
 #include <CurieBLE.h>
+#include "BleMidiEncoder.h"
 
 // Select fingering here
 #define FINGERING FINGERING_GAITA_GALEGA
@@ -31,22 +32,23 @@
 #define LED_PIN LED_BUILTIN
 #define LED_ACTIVE HIGH
 
-#define BLE_PACKET_SIZE 20
-
 // BLE MIDI
 BLEService midiSvc("03B80E5A-EDE8-4B33-A751-6CE34EC4C700");
 BLECharacteristic midiChar("7772E5DB-3868-4112-A1A9-F2669D106BF3",
-    BLEWrite | BLEWriteWithoutResponse | BLENotify | BLERead, BLE_PACKET_SIZE);
+    BLEWrite | BLEWriteWithoutResponse | BLENotify | BLERead,
+    BLE_MIDI_PACKET_SIZE);
 
+class CurieBleMidiEncoder: public BleMidiEncoder {
+  boolean setValue(const unsigned char value[], unsigned char length) {
+    return midiChar.setValue(value, length);
+  }
+};
+
+CurieBleMidiEncoder encoder;
 unsigned long previousFingers;
 uint8_t previousNote;
 boolean playing;
 boolean connected;
-
-uint8_t midiData[BLE_PACKET_SIZE];
-int byteOffset = 0;
-uint8_t lastStatus;
-uint32_t lastTime;
 
 void setup() {
   previousFingers = 0xFF;
@@ -70,15 +72,6 @@ void loop() {
   }
 }
 
-//void testNotes() {
-//  noteOn(CHANNEL, 60, 127);
-//  midiChar.setValue(midiData, 5);
-//  delay(200);
-//  noteOff(CHANNEL, 60);
-//  midiChar.setValue(midiData, 5);
-//  delay(400);
-//}
-
 void readFingers() {
   unsigned long fingers = OpenPipe.readFingers();
 
@@ -93,15 +86,15 @@ void readFingers() {
       // If note changed...
       if (OpenPipe.note != previousNote && OpenPipe.note != 0xFF) {
         // Stop previous note and start current
-        loadNoteOff(CHANNEL, previousNote);
-        loadNoteOn(CHANNEL, OpenPipe.note, 127);
-        sendMessages();
+        encodeNoteOff(CHANNEL, previousNote);
+        encodeNoteOn(CHANNEL, OpenPipe.note, 127);
+        encoder.sendMessages();
         previousNote = OpenPipe.note;
       }
     } else {
       if (playing) {
-        loadNoteOff(CHANNEL, previousNote); // Stop the note
-        sendMessages();
+        encodeNoteOff(CHANNEL, previousNote); // Stop the note
+        encoder.sendMessages();
         playing = false;
       }
     }
@@ -135,56 +128,16 @@ void setupBle() {
   midiSvc.addCharacteristic(midiChar);
 
   // set an initial value for the characteristic
-  sendMessage(0, 0, 0);
+  encoder.sendMessage(0, 0, 0);
 
   BLE.advertise();
 }
 
-boolean loadNoteOn(uint8_t channel, uint8_t note, uint8_t volume) {
-  return loadMessage(NOTEON | channel, note & 0x7F, volume & 0x7F);
+boolean encodeNoteOn(uint8_t channel, uint8_t note, uint8_t volume) {
+  return encoder.appendMessage(NOTEON | channel, note & 0x7F, volume & 0x7F);
 }
 
-boolean loadNoteOff(uint8_t channel, uint8_t note) {
-  return loadMessage(NOTEOFF | channel, note & 0x7F, 0);
-}
-
-boolean loadMessage(uint8_t status, uint8_t byte1, uint8_t byte2) {
-  // Assert BLE_PACKET_SIZE > 4
-  if (byteOffset > BLE_PACKET_SIZE - 4) return false;
-  uint32_t timestamp = millis();
-  boolean empty = byteOffset == 0;
-  if (empty) {
-    uint8_t headTs = timestamp >> 7;
-    headTs |= 1 << 7;  // set the 7th bit
-    headTs &= ~(1 << 6);  // clear the 6th bit
-    midiData[byteOffset++] = headTs;
-  }
-  if (empty || lastStatus != status || lastTime != timestamp) {
-    uint8_t msgTs = timestamp;
-    msgTs |= 1 << 7;  // set the 7th bit
-    midiData[byteOffset++] = msgTs;
-    midiData[byteOffset++] = status;
-    midiData[byteOffset++] = byte1;
-    midiData[byteOffset++] = byte2;
-    lastStatus = status;
-    lastTime = timestamp;
-  } else {
-    midiData[byteOffset++] = byte1;
-    midiData[byteOffset++] = byte2;
-  }
-  return true;
-}
-
-boolean sendMessage(uint8_t status, uint8_t byte1, uint8_t byte2) {
-  return loadMessage(status, byte1, byte2) && sendMessages();
-}
-
-boolean sendMessages() {
-  if (byteOffset != 0) {
-    midiChar.setValue(midiData, byteOffset);
-    byteOffset = 0;
-    return true;
-  }
-  return false;
+boolean encodeNoteOff(uint8_t channel, uint8_t note) {
+  return encoder.appendMessage(NOTEOFF | channel, note & 0x7F, 0);
 }
 
